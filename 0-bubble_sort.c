a95c69b5ac6e909d7807dc6aaac39282c560bfbd
Suntha Lucas <155491378+Sunnilu@users.noreply.github.com>
2024-04-09 14:37:20 -0500

    update to bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size) {
-    for (size_t i = 0; i < size - 1; i++) {
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                // Swap array[j] and array[j + 1]
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+swap_ints(int *a, int *b)
 
-                // Print the array after each swap
-                for (size_t k = 0; k < size; k++) {
-                    printf("%d ", array[k]);
-                }
-                printf("\n");
+int tmp;
+
+tmp = *a;
+*a = *b;
+*b = tmp;
+
+/**
+bubble_sort - sort an array of integers in ascending order
+* @array: an array of integers to sort
+* @size: the size of the array
+*/
+void bubble_sort(int *array, size_t size) 
+{
+    size_t, len = size;
+    bool bubbly = false;
+
+    if (array== NULL, || size < 2)
+            return;
+    while (bubbly == false)
+    {
+        bubbly = true;
+        for (i = 0, i < len - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap_ints(array + i,array + i + 1);
+                print_array(array, size);
+                bubbly = false;
             }
         }
+        len--;
     }
 }
 
-int main() {
-    int array[] = {64, 34, 25, 12, 22, 11, 90};
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    bubble_sort(array, size);
-
-    return 0;
+        }
+    }
 }
 
