5b29bc4bda3cc0ccbfceeda72201c54a6bdbfdf8
root <Suntha.Lucas@atlasschool.com>
2024-04-13 18:15:07 +0000

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,29 +1,38 @@
 #include "sort.h"
 
 /**
-* swap_ints - swap two integers in an array
-* @a: the first integer to swap
-* @b: the second interger to swap
-*/
-void swap_ints(int *a, int *b)
+ * selection_sort - sort a given array using the bubble sort algorithm in the ascending order.
+ * @array: the array to be sorted
+ * @size: the size of array
+ */
+void selection_sort(int *array, size_t size)
 {
-    inttmp;
+	size_t min_value_index = 0;
+	size_t pass = 0, index = 0;
+	size_t passes_required = 0;
+	int temp = 0;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
-/**
-* selection_sort - sort an array of integers in ascending order using the shell sort algorithm
-*@array: an array of integers
-*@size: the size of the array
-
-description: uses the knuth interval sequence
-*/
-void shell_sort(int *array, size_t size)
-{
-    size_t gap, i,j;
-
-    if(array == NULL || size < 2)
-    
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	passes_required = size - 1;
+	for (pass = 0; pass < passes_required; pass++)
+	{
+		min_value_index = pass;
+		for (index = pass + 1; index < size; index++)
+		{
+			if (array[index] < array[min_value_index])
+			{
+				min_value_index = index;
+			}
+		}
+		temp = array[pass];
+		array[pass] = array[min_value_index];
+		array[min_value_index] = temp;
+		if (min_value_index > pass)
+		{
+			print_array(array, size);
+		}
+	}
 }
