6f15a2a47525e2a8df8c80ac64fb6ab50f512738
root <Suntha.Lucas@atlasschool.com>
2024-04-13 17:53:52 +0000

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,55 +1,51 @@
 #include "sort.h"
 
 /**
-* swap_nodes - swap two nodes in a lisint_t doubly-linked list
-* @h: a pointer to the head of the doubly-linked list
-* @n1: a pointer to the first node to swap
-* @n2: the second node to swap
-*/
-void swap_nodes(listint_t **h,listint_t **n1, listint_t *n2)
-{
-    (*n1)->next = n2->next;
-    if (n2->next != NULL)
-        n2->next->prev = *n1;
-    n2->prev = (*n1)->prev;
-    n2->next = *n1;
-
-    if ((*n1)->prev != NULL)
-        (*n1)->prev->next = n2;
-
-    else
-        *h = n2;
-
-    (*n1)->prev = n2;
-    *n1 = n2->prev;
-}
-/**
-* insertion_sort_list - sorts a doubly linked list of integers using the insertion sort algorithm
-* @list: a pointer to the head of a doubly linked list of integers
-* description: prints the list after each swap
+* insertion_sort_list - sort a given array using insertion
+* sort algorithm in asending order
+* @list: the list to sort
 */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *tmp;
-    int n;
+	listint_t *sorted = NULL, *unsorted = NULL, *temp = NULL;
 
-    if (!list)
-	    return;
-    tmp = *list;
-    while (tmp)
-    {
-	if (tmp->next)
-    	{
-		if (tmp->n > tmp->next->n)
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	unsorted = (*list)->next; 
+	while (unsorted != NULL)
+	{
+		sorted = unsorted->prev;
+		temp = unsorted->next;
+		while (sorted != NULL)
 		{
-			n = tmp->n;
-			*(int *)&tmp->n = tmp->next->n;
-			*(int *)&tmp->next->n = n;
-			tmp = *list;
-			print_list(*list);
-			break;
+			if (unsorted->n < sorted->n)
+			{
+				sorted = sorted->prev;
+				if (unsorted->next != NULL)
+				{
+					unsorted->next->prev = 
+					unsorted->prev;
+				}
+				unsorted->prev->next = unsorted->next;
+				unsorted->prev = sorted;
+				if (sorted == NULL)
+				{
+					(*list)->prev = unsorted;
+					unsorted->next = *list;
+					*list = unsorted;
+				}
+				else
+				{
+					sorted->next->prev = unsorted;
+					unsorted->next = sorted->next;
+					sorted->next = unsorted;
+				}
+				print_list(*list);
+			}
+			else
+				break;
 		}
+		unsorted = temp;
 	}
-	tmp = tmp->next;
-     }
-}    
+}
+
